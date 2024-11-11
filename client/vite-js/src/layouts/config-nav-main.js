98152b583f2f4894032ab4e73cab4fc60c99c630
Burak <burakd279@gmail.com>
2024-11-11 11:13:21 +0000

    11.11.2024

--- client/vite-js/src/layouts/config-nav-main.js
+++ client/vite-js/src/layouts/config-nav-main.js
@@ -1,10 +1,8 @@
 import { paths } from 'src/routes/paths';
 import { CONFIG } from 'src/config-global';
 
-// Resim yolunu oluşturma fonksiyonu
 const imagePath = (name) => `${CONFIG.assetsDir}/assets/images/ürünler/${name}`;
 
-// Sayfa bağlantıları
 export const pageLinks = [
   {
     subheader: 'Anasayfa',
@@ -19,17 +17,23 @@ export const pageLinks = [
   {
     subheader: 'Ürünlerimiz',
     items: [
-      { title: 'E-Müstahsil Makbuzu', path: paths.urunler.emustahsil },
-      { title: 'E-Fatura', path: paths.urunler.efatura },
-      { title: 'E-Arşiv Fatura', path: paths.urunler.earsivfatura },
-      { title: 'E-İrsaliye', path: paths.urunler.eirsaliye },
-      { title: 'E-Serbest Meslek Makbuzu', path: paths.urunler.eserbestmeslekmakbuzu },
+      { 
+        title: 'E-Belge',
+        subItems: [
+         { title: 'E-Fatura', path: paths.urunler.efatura }, 
+         { title: 'E-Arşiv Fatura', path: paths.urunler.earsivfatura },
+         { title: 'E-İrsaliye', path: paths.urunler.eirsaliye },
+         { title: 'E-Serbest Meslek Makbuzu', path: paths.urunler.eserbestmeslekmakbuzu },
+         { title: 'E-Müstahsil Makbuzu', path: paths.urunler.emustahsil }, 
+         
+        ]
+      },
       { title: 'E-Defter', path: paths.urunler.edefter },
       { title: 'E-Saklama', path: paths.urunler.esaklama },
       { title: 'E-İmza', path: paths.urunler.eimza },
       { title: 'Kep', path: paths.urunler.kep },
-      { title: 'Beyan', path: paths.urunler.beyan},
-      { title: 'Aktarım', path: paths.urunler.aktarım},
+      { title: 'Beyan', path: paths.urunler.beyan },
+      { title: 'Aktarım', path: paths.urunler.aktarim },
     ],
   },
   {
@@ -49,49 +53,41 @@ export const pageLinks = [
   },
 ];
 
-// Ürünler için görsel ve başlık bağlantıları
 export const pageLinks2 = [
   {
-    items: [{ title: 'E-Müstahsil Makbuzu', path: paths.urunler.emustahsil }],
+    title: 'E-Belge',
+    items: [
+      { title: 'E-Müstahsil Makbuzu', path: paths.urunler.emustahsil },
+      { title: 'E-Fatura', path: paths.urunler.efatura },
+      { title: 'E-Arşiv Fatura', path: paths.urunler.earsivfatura },
+      { title: 'E-İrsaliye', path: paths.urunler.eirsaliye },
+      { title: 'E-Serbest Meslek Makbuzu', path: paths.urunler.eserbestmeslekmakbuzu },
+      { title: 'E-Defter', path: paths.urunler.edefter },
+    ]
   },
   {
-    items: [{ title: 'E-Fatura', path: paths.urunler.efatura }],
+    title: 'E-Saklama', path: paths.urunler.esaklama,
+    items: [
+      { title: 'E-Saklama', path: paths.urunler.esaklama },
+      { title: 'E-İmza', path: paths.urunler.eimza },
+      { title: 'Kep', path: paths.urunler.kep },
+      { title: 'Beyan', path: paths.urunler.beyan },
+      { title: 'Aktarım', path: paths.urunler.aktarim },
+    ],
   },
-  {
-    items: [{ title: 'E-Arşiv Fatura', path: paths.urunler.earsivfatura }],
-  },
-  {
-    items: [{ title: 'E-İrsaliye', path: paths.urunler.eirsaliye }],
-  },
-  {
-    items: [{ title: 'E-Serbest Meslek Makbuzu', path: paths.urunler.eserbestmeslekmakbuzu }],
-  },
-  {
-    items: [{ title: 'E-Defter', path: paths.urunler.edefter }],
-  },
-  {
-    items: [{ title: 'E-Saklama', path: paths.urunler.esaklama }],
-  },
-  {
-    items: [{ title: 'E-İmza', path: paths.urunler.eimza }],
-  },
-  {
-    items: [{ title: 'Kep', path: paths.urunler.kep }],
-  },
-  {
-    items: [{ title: 'Beyan', path: paths.urunler.beyan }],
-  },
-  {
-    items: [{ title: 'Aktarım', path: paths.urunler.aktarım }],
-  },
+  
 ];
 
-// Navigasyon verileri
+
 export const navData = [
   { title: 'Anasayfa', path: '/' },
   { title: 'Hakkımızda', path: '/company/about' },
-  { title: 'Ürünlerimiz', path: paths.pages, children: pageLinks2 },
+  { 
+    title: 'Ürünlerimiz', 
+    path: paths.pages,
+    children: pageLinks2
+  },
   { title: 'İletişim', path: '/company/contact' },
   { title: 'Blog', path: '/company/blog' },
-  // { title: 'İnsan Kaynakları', path: '/company/İnsanKaynaklari' },
 ];
+
